cd9cd8e4653685f9e3bf73e87293d991da8a6f44
CodebyKumar <153998849+CodebyKumar@users.noreply.github.com>
2025-02-25 14:37:32 +0530

    Basics

--- main.py
+++ main.py
@@ -1,9 +1,41 @@
 from fastapi import FastAPI
-
+from typing import Optional
+from pydantic import BaseModel
+import uvicorn
 
 app= FastAPI()
 
 
-@app.get('/')
-def index():
-    return {'data':{'name': 'Kumar'}}
+@app.get('/blog')
+def index(limit: int = 10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blogs from db'}
+    return {'data':f'{limit} blogs from db'}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+@app.get('/blog/{id}')
+def show(id: int):
+    # fetch blog by id = id
+    return {'data': id}
+
+@app.get('/bog/{id}/comments')
+def comments(id: int):
+    # feftch comments by id = id
+    return {'data': {'comments': id}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published_at: Optional[bool]
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return {'data': f'Blog is created with title as {blog.title}'}
+
+# debugging
+# if __name__ =='__main__':
+#     uvicorn.run(app, host='127.0.0.1', port=9000)
